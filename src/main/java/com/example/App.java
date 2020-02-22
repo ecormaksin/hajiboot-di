3739bb3409a52fd762e87f5afec0102e0dcb6d86
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-02-22 19:13:42 +0900

    「Appクラス」内で「ArgumentResolver」を「DIコンテナ」から取得するように修正

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,25 +1,24 @@
 package com.example;
 
-import java.util.Scanner;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
 import com.example.app.Calculator;
 
 @EnableAutoConfiguration
 @Import(AppConfig.class) // (1)
 public class App {
     public static void main(String[] args) {
-        try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args); /* (2) */
-                Scanner scanner = new Scanner(System.in);) {
+        try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
             System.out.print("Enter 2 numbers like 'a b' : "); // (3)
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
+            ArgumentResolver argumentResolver = context.getBean(ArgumentResolver.class);
+            Argument argument = argumentResolver.resolve(System.in);
             Calculator calculator = context.getBean(Calculator.class); // (4)
-            int result = calculator.calc(a, b);
+            int result = calculator.calc(argument.getA(), argument.getB());
             System.out.println("result = " + result);
         }
     }
