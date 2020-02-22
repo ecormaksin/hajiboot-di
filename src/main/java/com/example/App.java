e727c6c727208b0c2030c31ca5d19336584f3bcc
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-02-22 19:23:26 +0900

    [2.1.3] 「オート・ワイヤリング」によるDI

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,22 +5,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 
-import com.example.app.Argument;
-import com.example.app.ArgumentResolver;
-import com.example.app.Calculator;
+import com.example.app.Frontend;
 
 @EnableAutoConfiguration
 @Import(AppConfig.class) // (1)
 public class App {
     public static void main(String[] args) {
         try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
-            System.out.print("Enter 2 numbers like 'a b' : "); // (3)
-            ArgumentResolver argumentResolver = context.getBean(ArgumentResolver.class);
-            Argument argument = argumentResolver.resolve(System.in);
-            Calculator calculator = context.getBean(Calculator.class); // (4)
-            int result = calculator.calc(argument.getA(), argument.getB());
-            System.out.println("result = " + result);
+            Frontend frontend = context.getBean(Frontend.class);
+            frontend.run();
         }
     }
-
 }
